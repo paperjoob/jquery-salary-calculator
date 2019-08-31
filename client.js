16eed36262210c62abfd71ca6699b4ad2930b2b6
See Yang <seeyang24@gmail.com>
2019-08-31 16:35:50 -0500

    able to add in new employee information

--- client.js
+++ client.js
@@ -1,14 +1,75 @@
 $(document).ready(readyNow);
 
+const allEmployees = [
+    {
+        firstName: 'Jen', 
+        lastName: 'Barber', 
+        id: 4521, 
+        title: 'Team Lead', 
+        annualSalary: `$80,000`
+    },
+    {
+        firstName: 'Maurice', 
+        lastName: 'Moss', 
+        id: 8724, 
+        title: 'Support Team', 
+        annualSalary: `$58,000`
+    },
+    {
+        firstName: 'Roy', 
+        lastName: 'Smith', 
+        id: 9623, 
+        title: 'Quality Assurance', 
+        annualSalary: `$48,000`
+    }
+];
+
 function readyNow() {
     appendDom();
-    console.log('hello');
+    console.log('in ReadyNow');
+    $('#submitButton').on('click', handleSubmit);
 }
 
 function appendDom() {
-    let header = $('<h1>Salary Calculator</h1>');
-    $('.container').append(header);
+    console.log('in appendDom');
+    $('#empTable').empty();
+    $('#tableBody').empty();
 
-    $('<p>Add Employee</p>');
+    let table = $(`<table id='empTable'></table>`);
+    table.append('<thead><tr><th>First Name</th><th>Last Name</th><th>ID</th><th>Title</th><th>Annual Salary</th><th>Delete</th></thead>');
 
+    let tbody = $('<tbody id="tableBody"></tbody>');
+    table.append(tbody);
+
+    $('.employeeTable').append(table);
+
+    allEmployees.forEach(function(employee) {
+        let empName = $(`<tr><td>` + employee.firstName + `<td> ` + employee.lastName  + ` <td>` + employee.id + `<td>` + employee.title  + ` <td> ` + employee.annualSalary  +  `<td>` + `<button class=deleteButton'>Delete</button>` + `<td></tr>`);
+        $('#tableBody').append(empName );
+    })
 }
+
+function handleSubmit() {
+    let firstNameVal = $('#firstNameIn').val();
+    let lastNameVal = $('#lastNameIn').val();
+    let idInVal = $('#idIn').val();
+    let titleInVal = $('#titleIn').val();
+    let annualSalaryInVal= $('#annualSalaryIn').val();
+
+    let newEmployee = {
+        firstName: firstNameVal,
+        lastName: lastNameVal,
+        id: idInVal,
+        title: titleInVal,
+        annualSalary: annualSalaryInVal
+    }
+
+    allEmployees.push(newEmployee);
+    $('#firstNameIn').val('');
+    $('#lastNameIn').val('');
+    $('#idIn').val('');
+    $('#titleValIn').val('');
+    $('#annualSalaryIn').val('');
+    appendDom();
+}
+
