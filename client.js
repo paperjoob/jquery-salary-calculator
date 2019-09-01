d50d1983211bd78776d40657d5db3c194f8d9f75
See Yang <seeyang24@gmail.com>
2019-09-01 13:31:18 -0500

    finished base

--- client.js
+++ client.js
@@ -6,47 +6,45 @@ const allEmployees = [
         lastName: 'Barber', 
         id: 4521, 
         title: 'Team Lead', 
-        annualSalary: `$80,000`
+        annualSalary: 80000
     },
     {
         firstName: 'Maurice', 
         lastName: 'Moss', 
         id: 8724, 
         title: 'Support Team', 
-        annualSalary: `$58,000`
+        annualSalary: 58000
     },
     {
         firstName: 'Roy', 
         lastName: 'Smith', 
         id: 9623, 
         title: 'Quality Assurance', 
-        annualSalary: `$48,000`
+        annualSalary: 48000
     }
 ];
 
 function readyNow() {
     appendDom();
-    console.log('in ReadyNow');
+    addMonthlyCosts();
     $('#submitButton').on('click', handleSubmit);
 }
 
 function appendDom() {
     console.log('in appendDom');
-    $('#empTable').empty();
     $('#tableBody').empty();
-
-    let table = $(`<table id='empTable'></table>`);
+    let table = $('#empTable');
+    table.empty();
     table.append('<thead><tr><th>First Name</th><th>Last Name</th><th>ID</th><th>Title</th><th>Annual Salary</th><th>Delete</th></thead>');
-
     let tbody = $('<tbody id="tableBody"></tbody>');
     table.append(tbody);
-
     $('.employeeTable').append(table);
 
     allEmployees.forEach(function(employee) {
-        let empName = $(`<tr><td>` + employee.firstName + `<td> ` + employee.lastName  + ` <td>` + employee.id + `<td>` + employee.title  + ` <td> ` + employee.annualSalary  +  `<td>` + `<button class=deleteButton'>Delete</button>` + `<td></tr>`);
-        $('#tableBody').append(empName );
+        let empName = $(`<tr><td>` + employee.firstName + `<td> ` + employee.lastName  + ` <td>` + employee.id + `<td>` + employee.title  + ` <td> ` + '$' + employee.annualSalary  +  `<td>` + `<button class="deleteButton">Delete</button>` + `<td></tr>`);
+        $('#tableBody').append(empName);
     })
+    $('.deleteButton').on('click', deleteEmp); // delete the parent's parent of the deleteButton
 }
 
 function handleSubmit() {
@@ -57,19 +55,48 @@ function handleSubmit() {
     let annualSalaryInVal= $('#annualSalaryIn').val();
 
     let newEmployee = {
-        firstName: firstNameVal,
+        firstName: firstNameVal, // assign firstname to the value of the firstName
         lastName: lastNameVal,
         id: idInVal,
         title: titleInVal,
         annualSalary: annualSalaryInVal
     }
-
-    allEmployees.push(newEmployee);
-    $('#firstNameIn').val('');
+    allEmployees.push(newEmployee); // push new employees into allEmployees array
+    $('#firstNameIn').val(''); // clears out input field after submission
     $('#lastNameIn').val('');
     $('#idIn').val('');
     $('#titleValIn').val('');
     $('#annualSalaryIn').val('');
+    addMonthlyCosts();
     appendDom();
+} // end handleSubmit
+
+function addMonthlyCosts(salary) {
+    console.log('in addMonthlyCosts');
+    let totalMonthlySpend = 0;
+    for (employee of allEmployees) {
+        totalMonthlySpend += employee.annualSalary / 12;
+    } // end for loop
+    let totalEl = $('#totalMonthlyOut');
+    totalEl.empty();
+    totalEl.append(totalMonthlySpend);
+    $('#totalMonthlyOut').html(totalMonthlySpend);
+
+    if (totalMonthlySpend > 20000){
+        $('#totalMonthlyOut').addClass('red');
+    } // end if more than 20,000
 }
 
+function deleteEmp() {
+    console.log('in deleteEmp');
+    $(this).parent().parent().remove();
+    let deleteItem = employee;
+    allEmployees.splice($.inArray(deleteItem, employee), 1); //access employee and remove 1 object in the array
+    removeSalary();
+} // end deleteEmp
+
+function removeSalary() {
+    console.log('in removeSalary');
+
+
+}
